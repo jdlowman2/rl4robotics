90d9f981ef07e862ddacea335645471e47c5b5bb
Joseph Lowman <lowmanj@umich.edu>
2019-12-07 19:22:36 -0500

    Removed unused files

--- ddpg/load_and_plot_experiment.py
+++ ddpg/load_and_plot_experiment.py
@@ -5,33 +5,60 @@ from pathlib import Path
 import torch
 from matplotlib import pyplot as plt
 import os
+import time
 
 import IPython
 
-# Change these values----------------
-foldername = "experiments/" + "Lander Converge/"
-save_intervals = list(range(0, 25001, 1000))
+# import sys
+# if "win" in sys.platform: # My Local Windows Machine
+#     sys.path.append("C:/Users/jdlow/Box/Fall 2019/ROB 590 RL for Driving/collision_environment/gym-lane-change/gym_lane_change/envs/")
+# else: # Great Lakes Linux platfrom
+#     sys.path.append("collision_environment/gym-lane-change/gym_lane_change/envs/")
+# from lane_change_env import LaneChangeEnv
+
+# Change foldername
+foldername = "experiments/" + "p_defaults_Pen_12_4_17_37/"
+
 render = False
-#------------------------------------
+episodes_per_file = 25 # number of episodes to run. The mean and stdev will be computed over these
+underscore_in_experiment_name = True # eg. m2_report
+ylimits = None
+
+# save_intervals = list(range(0, 2501, 100))
+saved_episode_nums = sorted([int(file.split("_")[1]) for file in os.listdir(foldername) if "actor" in file])
+max_episode_num = max(saved_episode_nums)
+IPython.embed()
+step_size = saved_episode_nums[1] - saved_episode_nums[0]
+save_intervals = list(range(0, max_episode_num+1, int(step_size)))
+
 
 environments = {"pen": "Pendulum-v0",
                     "lun": "LunarLanderContinuous-v2",
                     "mou": "MountainCarContinuous-v0",
+                    "lan": "LaneChangeEnv",
+                    "bip": "BipedalWalker-v2",
                     }
 save_filenames = [file for file in os.listdir(foldername) if "actor" in file]
 
 # assert(len(save_intervals) == len(save_filenames))
 file = save_filenames[0].split('_')
 total_num = file[3]
-exp_name = file[4]
-envname = environments[file[5].lower()]
+if underscore_in_experiment_name:
+    exp_name = "_".join(file[4:6])
+    envname = environments[file[6].lower()]
+else:
+    exp_name = file[4]
+    envname = environments[file[5].lower()]
+
 start_time = "_".join(file[-4:]).strip("actor")
 
-episodes_per_file = 100 # number of episodes to run. The mean and stdev will be computed over these
 L1_SIZE = 400
 L2_SIZE = 300
 
-env = gym.make(envname)
+if envname.lower() == "lanechangeenv":
+    env = LaneChangeEnv()
+else:
+    env = gym.make(envname)
 
 save_num_rewards = np.zeros((episodes_per_file, len(save_intervals)))
 
@@ -45,7 +72,15 @@ for save_ind, save_num in enumerate(save_intervals):
     actor = Actor(env.observation_space.shape[0],
                         env.action_space, L1_SIZE, L2_SIZE)
 
-    actor.load_state_dict(torch.load(experiment_path))
+    t1 = time.time()
+    try:
+        actor.load_state_dict(torch.load(experiment_path))
+    except FileNotFoundError:
+        print("\nFile not found: ", experiment_path)
+        print("Saving results so far\n")
+        break
+    t2 = time.time()
+    print("Time to load actor ", round(t2 - t1, 3))
 
     for ind, episode in enumerate(list(range(episodes_per_file))):
         # print("Reset")
@@ -62,6 +97,8 @@ for save_ind, save_num in enumerate(save_intervals):
             # print("reward: ", reward, " action: ", action)
 
         save_num_rewards[ind, save_ind] = rewards
+    print("Time to test ", round(time.time() - t2, 3))
+
 
     assert(np.nonzero(save_num_rewards[:, save_ind])[0].shape[0] == save_num_rewards[:, save_ind].shape[0])
 
@@ -71,6 +108,10 @@ for save_ind, save_num in enumerate(save_intervals):
 
 env.close()
 
+save_file = foldername + "/" + "eval_rewards_eps_by_intervals"
+np.save(save_file, save_num_rewards)
+print("Results saved to ", save_file)
+
 means = [row.mean() for row in save_num_rewards.T]
 stdevs = [np.std(row) for row in save_num_rewards.T]
 
@@ -78,6 +119,8 @@ plt.errorbar(save_intervals, means, yerr=stdevs,
             label=envname, fmt="-o", linewidth=2.0, ecolor="red", capsize=10.0)
 plt.xlabel("Number of Training Episodes")
 plt.ylabel("Rewards Per Episode for 100 Episodes")
+if ylimits is not None:
+    plt.ylim(ylimits)
 plt.legend()
 plt.tight_layout()
 # plt.show()
@@ -85,4 +128,4 @@ plt.savefig(foldername +  foldername.split("/")[-2].lower().replace(" ", "_") + 
         ".png", dpi=200)
 plt.close()
 
-IPython.embed()
+# IPython.embed()
